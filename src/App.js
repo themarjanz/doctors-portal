a664b52ea053409d86f52a0e5bd8d87d65c73159
themarjanz <themarjanz@gmail.com>
2022-10-10 22:10:58 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Navbar from './Pages/Shared/Navbar';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="about" element={<About></About>} />
+      </Routes>
+    </div>
+
+
+  );
+}
+
+export default App;
