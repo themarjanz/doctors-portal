ab48a43c3d431802dd20fad1f56e89362ec2399b
themarjanz <themarjanz@gmail.com>
2022-10-11 11:16:14 +0600

    info-added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BAnner from './BAnner';
+import Info from './Info';
 
 const Home = () => {
     return (
         <div>
             <BAnner></BAnner>
+            <Info></Info>
         </div>
     );
 };
